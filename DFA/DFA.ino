9982cc85d68200ed556b92661c19ad377cea4958
Maia Briggs <maia@briggs.nz>
2024-03-26 11:20:07 +0000

    Updated pin and hardware information

--- DFA/DFA.ino
+++ DFA/DFA.ino
@@ -3,6 +3,28 @@
 #include <Movements.h>
 #include <RoboClaw.h>
 
+//RoboClaw ----- pin S1 -> pin 13, pin S2 -> pin 12
+//Pin assignments for Sensors
+int fTrig = 7;
+int fEcho = 6;
+
+int m2Trig = 9;
+int m2Echo = 8;
+
+int m1Trig = 3;
+int m1Echo = 2;
+
+int fm2Trig = 11;
+int fm2Echo = 10;
+
+int fm1Trig = 5;
+int fm1Echo = 4;
+
+
+
+FiveSensors fiveSensors(fTrig, fEcho, m2Trig, m2Echo, m1Trig, m1Echo, fm1Trig, fm1Echo, fm2Trig, fm2Echo);
+Movements movements; 
+
 void setup() {
   // put your setup code here, to run once:
 
